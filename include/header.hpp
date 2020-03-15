903c9499c2595f776a36f496db6417e89f1e19c7
kelril <kelril@dsu1.service.sarnet>
2020-03-15 22:00:54 +0300

    0.1.2

--- include/header.hpp
+++ include/header.hpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <vector>
 #include <mutex>
-#include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/posix_time/posix_time_io.hpp>
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <boost/thread/thread.hpp>
